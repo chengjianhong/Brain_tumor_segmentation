4b97caf56d34c9ab8d453bafedf39bb36ebdd351
chengjianhong <1552648502@qq.com>
2018-12-12 21:30:41 +0800

    commit

--- io/load_data.py
+++ io/load_data.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import nibabel as nib
+import numpy as np
 
 import os
 
@@ -9,5 +10,12 @@ def read_NIFTI(file_path):
     nii_image = nii_image.get_data()
     return nii_image
 
-def train_test_split():
+def train_test_split(data,labels,te):
     return
+
+num=0
+np.random.seed(5)
+while(num<5):
+
+    print(np.random.random())
+    num+=1
